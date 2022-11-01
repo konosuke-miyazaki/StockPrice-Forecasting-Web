bd0301049aa9fb2f688cc8891cd5be6286693cf9
宮崎皓之介 <j1121280@icloud.com>
2022-11-01 21:11:23 +0900

    csvに曜日項目を追加 matplotlibで描画できることを確認

--- scraping/stooq_scraping.py
+++ scraping/stooq_scraping.py
@@ -1,6 +1,8 @@
+from calendar import weekday
 import os
 import datetime as dt
 import pandas_datareader.data as web
+import matplotlib.pyplot as plt
 
 # 銘柄コード入力(7177はGMO-APです。)
 ticker_symbol = "7177"
@@ -13,8 +15,19 @@ end = dt.date.today()
 # データ取得
 df = web.DataReader(ticker_symbol_dr, data_source='stooq',
                     start=start, end=end)
+# データに曜日を追加
+df['weekday'] = df.index.weekday
 # 2列目に銘柄コード追加
 df.insert(0, "code", ticker_symbol, allow_duplicates=False)
+
+plt.figure(figsize=(10, 6))
+plt.plot(df['Close'], label='Close', color='orange')
+plt.xlabel('Date')
+plt.ylabel('USD')
+plt.legend()
+plt.show()
+
+
 # csv保存
-df.to_csv(os.path.dirname(__file__) +
-          '/scraping_data/s_stock_data_' + ticker_symbol + '.csv')
+# df.to_csv(os.path.dirname(__file__) +
+#           '/scraping_data/s_stock_data_' + ticker_symbol + '.csv')

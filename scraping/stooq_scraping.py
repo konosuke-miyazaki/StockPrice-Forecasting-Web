f2f0c1fbe4070f0958bedd83d76af46767e4bb1e
宮崎皓之介 <j1121280@icloud.com>
2022-11-07 18:36:26 +0900

    取得したデータを整形して予想に使用できるように下準備

--- scraping/stooq_scraping.py
+++ scraping/stooq_scraping.py
@@ -17,15 +17,51 @@ df = web.DataReader(ticker_symbol_dr, data_source='stooq',
                     start=start, end=end)
 # データに曜日を追加
 df['weekday'] = df.index.weekday
+
+df.info()
+
 # 2列目に銘柄コード追加
 df.insert(0, "code", ticker_symbol, allow_duplicates=False)
 
-plt.figure(figsize=(10, 6))
-plt.plot(df['Close'], label='Close', color='orange')
-plt.xlabel('Date')
-plt.ylabel('USD')
-plt.legend()
-plt.show()
+# データの並び替え
+df.sort_values(by='Date', ascending=True, inplace=True)
+
+#カラム情報を1行上にずらしたデータフレームを作成する
+df_shift = df.shift(-1)
+df_shift
+
+#翌日の始値と本日の終値の差分を追加する
+df['delta_Close'] = df_shift['Close'] - df['Close']
+
+#目的変数Upを追加する(翌日の終値が上がる場合1、それ以外は0とする)、'delta_Close'カラムの削除
+df['Up'] = 0
+df['Up'][df['delta_Close'] > 0] = 1
+df = df.drop('delta_Close', axis=1)
+
+
+# 'Open', 'High', 'Low', 'Close'グラフ化のためにカラム抽出
+df_new = df[['Open', 'High', 'Low', 'Close']]
+
+# 時系列折れ線グラフの作成
+df_new.plot(kind='line')
+# plt.show()
+
+# 終値の前日比の追加
+df_shift = df.shift(1)
+
+df['Close_ratio'] = (df['Close'] - df_shift['Close']) / df_shift['Close']
+
+# 始値と終値の差分を追加
+df['Body'] = df['Open'] - df['Close']
+
+print(df)
+
+# plt.figure(figsize=(10, 6))
+# plt.plot(df['Close'], label='Close', color='orange')
+# plt.xlabel('Date')
+# plt.ylabel('USD')
+# plt.legend()
+# plt.show()
 
 
 # csv保存
